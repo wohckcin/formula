a3e2417ece36a4ae1c6a9a21daf7c9e62b21d501
Nick Chow <nicky.chow@gmail.com>
2012-05-09 17:25:31 +0800

    Add Users and Bootstraping

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
+# coding: utf-8
 class User < ActiveRecord::Base
 	rolify
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+         :recoverable, :rememberable, :trackable,
+         :validatable, :token_authenticatable,
+         :encryptable, :timeoutable
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence:true, uniqueness: { case_sensitive: false }
+
+
 end
